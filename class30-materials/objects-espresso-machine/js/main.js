5a4e927bcc8e2ea50264e0952c2a774c6e9ff602
mostafa rafati <mostafarafati888@gmail.com>
2022-05-08 03:59:03 +0430

    2

--- class30-materials/objects-espresso-machine/js/main.js
+++ class30-materials/objects-espresso-machine/js/main.js
@@ -1,4 +1,17 @@
 //Create an espresso machine constructor that makes machines with 4 properties and 3 methods
 class EspressoMachine {
-    constructor(color , )
+	constructor(color, cupNumber, brand, size) {
+		this.color = color;
+		this.cupNumber = cupNumber;
+		this.brand = brand;
+		this.size = size;
+	}
+	makeCoffie() {
+		console.log(`your ${this.cupNumber} cups are ready to drink`);
+	}
+	makeSteam() {
+		console.log(`care for steam of this ${this.size} ${this.constructor.name}`);
+	}
 }
+
+const albertoEspresso = new EspressoMachine('black', 2, 'xiaomi', 'big');
