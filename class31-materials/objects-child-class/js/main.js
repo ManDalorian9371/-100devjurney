88925ed63ac2f7b3b037fe4ee9c5e0162679c169
mostafa rafati <mostafarafati888@gmail.com>
2022-05-13 05:30:24 +0430

    3

--- class31-materials/objects-child-class/js/main.js
+++ class31-materials/objects-child-class/js/main.js
@@ -1,18 +1,34 @@
-//Create an a class and extend it - Can be anything you would like it to be!
-class Animal {
-	constructor(name, size) {
-		this.name = name;
-		this.size = size;
-	}
-	speak() {
-		console.log(`${this.name} makes a ${this.size} sound`);
+// //Create an a class and extend it - Can be anything you would like it to be!
+// class Animal {
+// 	constructor(name, size) {
+// 		this.name = name;
+// 		this.size = size;
+// 	}
+// 	speak() {
+// 		console.log(`${this.name} makes a ${this.size} sound`);
+// 	}
+// }
+
+// class Chicken extends Animal {
+// 	constructor(name, size, useFor) {
+// 		super(name, size);
+// 		this.usage = useFor;
+// 	}
+// }
+// let jojo = new Chicken('jojo', 'Small', 'Egg');
+function* fibonacci(){
+	// a generator function
+	let [ prev, curr ] = [ 0, 1 ];
+	while (true) {
+		[ prev, curr ] = [ curr, prev + curr ];
+		yield curr;
 	}
 }
 
-class Chicken extends Animal {
-	constructor(name, size, useFor) {
-		super(name, size);
-		this.usage = useFor;
+for (const n of fibonacci()) {
+	console.log(n);
+	// truncate the sequence at 1000
+	if (n >= 1000) {
+		break;
 	}
 }
-let jojo = new Chicken('jojo', 'Small', 'Egg');
